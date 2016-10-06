9396599c6ca51b2c637ff2ce209574da6179bd7b
doebo <doebo@DESKTOP-77TLRJ1>
2016-10-06 15:19:43 -0600

    functional gui, limited logic, functional cards

--- src/slapjack/Player.java
+++ src/slapjack/Player.java
@@ -8,13 +8,18 @@ public class Player {
     public boolean isPlaying; // whether or not the player is still in the round
     public ArrayList<Card> hand; // the player's hand
     public boolean slapped; // flips when a player has slapped the pile
-    public boolean isPlayersTurn; // determines if the player can turn their card
+    public boolean isPlayersTurn = false; // determines if the player can turn their card
+    private final int playerNumber;
     
     //instantiates the new hand
-    public Player(){
-        
+    public Player(int playerNumber){
+        this.playerNumber = playerNumber;
+        hand = new ArrayList<>();
     }
     
+    public int getPlayerNumber(){
+        return playerNumber;
+    }
     // if the player has cards,
     // gives a card from their hand to the player whose card they slapped.
     public Card giveCard(){
@@ -24,8 +29,15 @@ public class Player {
     
     
     //called when the player presses the key for slapping
+    //this method will set their slapped boolean to true
     public void slapCard(){
-        slapped = true;
+        if(slapped != true){
+            System.out.println(playerNumber + " slapped");
+            slapped = true;
+        }
+        else{
+            System.out.println(playerNumber + " has already slapped this turn");
+        }
     }
     
     // for shuffling the players hand when they've won a hand or if another player
@@ -36,6 +48,11 @@ public class Player {
     
     //only if it's the player's turn (isPlayersTurn == true)
     public Card turnCard(){
-        return hand.remove(0);
+        if(isPlayersTurn){
+            System.out.println(playerNumber + " turned their card");
+            isPlayersTurn = false;
+        } 
+        //return hand.remove(0); 
+        return null;
     }
 }
